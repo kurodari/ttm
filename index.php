fa5ef5572a2abcabd8793720a927e8238bcbee8a
kurodari <nanadari@nate.com>
2015-04-17 16:18:06 +0900

    임시 메인페이지 기본 틀 작성

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Tatoo Marketer</title>
+		<link type="text/css" href="./common.css" rel="stylesheet" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- 부트스트랩 -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+	
+	<body>
+		
+		<!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요한) -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- 모든 합쳐진 플러그인을 포함하거나 (아래) 필요한 각각의 파일들을 포함하세요 -->
+		<script src="js/bootstrap.min.js"></script>
+
+		<!-- Respond.js 으로 IE8 에서 반응형 기능을 활성화하세요 (https://github.com/scottjehl/Respond) -->
+		<script src="js/respond.js"></script>
+	</body>
+</html>	
